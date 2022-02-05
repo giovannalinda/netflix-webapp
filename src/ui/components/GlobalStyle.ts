2d9f74e61915aff06589e0b218e51a6b6792e2a7
Giovanna Linda <eugiovannasouza@gmail.com>
2022-02-05 01:16:08 -0300

    setup tests

--- src/ui/components/GlobalStyle.ts
+++ src/ui/components/GlobalStyle.ts
@@ -8,25 +8,31 @@ export const GlobalStyle = createGlobalStyle`
     outline: 0;
     box-sizing: border-box;
   }
+
   body {
     background: ${theme.colors.white};
     font-family: ${theme.font.family};
     overflow-x: hidden;
     color: ${theme.colors.black};
   }
+
   button {
     cursor: pointer;
   }
+
   a {
     text-decoration: none;
   }
+
   ul {
     list-style: none;
   }
+
   &::selection {
     color: ${theme.colors.neutral[100]};
     background: ${theme.colors.black};
   }
+
   html {
     font-size: 62.5%;
   }
